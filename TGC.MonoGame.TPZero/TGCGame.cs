aed25f2621ae0ec22881c60eadfbb28b76a8cdd4
Ramiro Villani <rvillani@frba.utn.edu.ar>
2024-08-27 00:05:40 -0300

    A

--- TGC.MonoGame.TPZero/TGCGame.cs
+++ TGC.MonoGame.TPZero/TGCGame.cs
@@ -77,6 +77,7 @@ namespace TGC.MonoGame.TP
             // Creo la escena de la ciudad.
             City = new CityScene(Content);
 
+
             // La carga de contenido debe ser realizada aca.
 
             base.LoadContent();
